cae2bf05902a6e8ce94e8e5b07c1e3bd876dca41
DoomKitty87 <doomkitty887@gmail.com>
2024-01-26 17:41:16 -0800

    some design

--- noisetest.js
+++ noisetest.js
@@ -6,45 +6,77 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+document.getElementById("renderercontainer").appendChild( renderer.domElement );
 
 const geometry = new THREE.BufferGeometry();
 
-const vertices = new Float32Array( [
-  -1.0, 0.0, 0.0,
-  0.0, 0.0, 0.0,
-  1.0, 0.0, 0.0,
-  -1.0, 0.0, 1.0,
-  0.0, 0.0, 1.0,
-  1.0, 0.0, 1.0
-]);
+const width = 250;
+const depth = 100;
 
-const indices = [
-  0, 1, 2,
-  3, 4, 5,
-  0, 3, 0,
-  1, 4, 1,
-  2, 5, 2,
-];
+const vertices = new Float32Array(width * depth * 3);
+
+const vertexScale = 1;
+
+for (var i = 0; i < width * depth; i++) {
+  vertices[i * 3] = (i % width - width / 2) * vertexScale;
+  vertices[i * 3 + 1] = 0;
+  vertices[i * 3 + 2] = -Math.floor(i / width) * vertexScale;
+}
+
+const indices = [];
+
+for (var x = 0; x < (width - 1); x++) {
+  for (var z = 0; z < (depth - 1); z++) {
+    indices.push(x + z * width);
+    indices.push(x + 1 + z * width);
+    indices.push(x + z * width);
+    indices.push(x + 1 + z * width);
+    indices.push(x + 1 + (z + 1) * width);
+    indices.push(x + (z + 1) * width);
+  }
+}
 
 const noise2D = createNoise2D();
 
 geometry.setIndex(indices);
-geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+var buffer = new THREE.BufferAttribute(vertices, 3);
+geometry.setAttribute('position', buffer);
 const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
 material.wireframe = true;
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const plane = new THREE.Mesh( geometry, material );
+scene.add( plane );
 
-camera.position.z = 5;
+var mousePos = {x: 0, y: 0};
 
+const resolutionX = window.innerWidth;
+const resolutionY = window.innerHeight;
+document.addEventListener('mousemove', function(e) {
+  mousePos.x = e.clientX / resolutionX * 2 - 1;
+  mousePos.y = e.clientY / resolutionY * 2 - 1;
+});
+
+camera.position.y = 5;
+const speed = 0.02;
+const delay = 1 / speed;
+const movementSensitivity = 0.1;
+const rotationSensitivity = 0.02;
+var frames = 0;
 function animate() {
 	requestAnimationFrame( animate );
-  for (int i = 0; i < vertices.length / 3; i++) {
-    vertices[i * 3 + 1] += noise2D(vertices[i * 3], vertices[i * 3 + 2]); 
+  if (frames % delay == 0) {
+    for (var i = 0; i < vertices.length / 3; i++) {
+      vertices[i * 3 + 2] -= vertexScale;
+      vertices[i * 3 + 1] = noise2D(vertices[i * 3] / 100, vertices[i * 3 + 2] / 40) * Math.abs(vertices[i * 3]) * 0.25; 
+    }
+    buffer.needsUpdate = true;
+    geometry.computeBoundingBox();
+    geometry.computeBoundingSphere();
   }
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  plane.position.x = -(mousePos.x) * movementSensitivity;
+  plane.position.y = (mousePos.y) * movementSensitivity;
+  camera.setRotationFromEuler(new THREE.Euler((mousePos.y) * -rotationSensitivity, (mousePos.x) * -rotationSensitivity, 0));
+  frames++;
+  plane.position.z = frames / delay * vertexScale;
 	renderer.render( scene, camera );
 }
 animate();
